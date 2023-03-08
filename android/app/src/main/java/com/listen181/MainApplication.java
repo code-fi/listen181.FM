40d9fda9f208027b9820009166639925e0c16153
Frank Owusu-Agyemang <codewithfrank@gmail.com>
2023-03-08 01:40:46 +0100

    patch: updated android build stuff

--- android/app/src/main/java/com/listen181/MainApplication.java
+++ android/app/src/main/java/com/listen181/MainApplication.java
@@ -52,7 +52,7 @@ public class MainApplication extends NavigationApplication {
   @Override
   public void onCreate() {
     super.onCreate();
-    
+
     if (BuildConfig.IS_NEW_ARCHITECTURE_ENABLED) {
       // If you opted-in for the New Architecture, we load the native entry point for this app.
       DefaultNewArchitectureEntryPoint.load();
